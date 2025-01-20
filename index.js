4267986c98bf68c93189dfec791712bbd19daa87
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-20 18:14:43 +0600

    JWT updated.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
